823aa47f8fcc602f4e653f87de65c3c1a7cb1cf2
Thiago Souto <thiagocsouto@outlook.com>
2022-06-24 13:44:17 -0400

    Delete evento/evento directory

--- evento/evento/target/classes/schema-h2.sql
+++
@@ -1,5 +0,0 @@
-create table evento(id integer auto_increment, nome varchar(50), local_evento varchar(50), data varchar(50), horario varchar(50));
-
-create table convidado(id integer auto_increment, nome varchar(50), rg varchar(15), idade integer); 
-
-create table usuario(id integer auto_increment, username varchar(50), password varchar(80)); 
