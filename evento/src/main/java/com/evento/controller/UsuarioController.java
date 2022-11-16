96814cdb69b52666cf4c7e46165646e7840a386d
Thiago Souto <thiagocsouto@outlook.com>
2022-11-16 09:32:15 -0500

    Add files via upload

---
+++ evento/src/main/java/com/evento/controller/UsuarioController.java
@@ -0,0 +1,65 @@
+package com.evento.controller;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.evento.model.Evento;
+import com.evento.model.Usuario;
+import com.evento.repository.UsuarioRepository;
+
+
+@Controller
+public class UsuarioController {
+	
+	@Autowired
+	private UserDetailsService usuarioService;
+	
+	@GetMapping("/login") 
+	public ModelAndView login() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("login/login");
+		mv.addObject("usuario", new Usuario());  
+		return mv;    
+	}
+	
+	
+	@GetMapping("/home") 
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("home/index");
+		mv.addObject("usuario", new Usuario());  
+		return mv;   
+	}
+	
+	@GetMapping("/adm/home") 
+	public ModelAndView indexAdm() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("home/index");
+		mv.addObject("usuario", new Usuario());  
+		return mv;   
+	}
+	
+	@RequestMapping("logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:login";
+	}
+	
+	
+}
+	
+	
+	
+
+
---
+++ evento/src/test/java/com/evento/EventoServiceTest.java
@@ -0,0 +1,5 @@
+package com.evento;
+
+public class EventoServiceTest {
+
+}
