70c2566d4eaa0abc267f064255e807001b3727de
Thiago Souto <thiagocsouto@outlook.com>
2022-11-16 09:29:55 -0500

    Delete evento directory

--- evento/src/main/java/com/evento/repository/ConvidadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.evento.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.evento.model.Convidado;
-
-public interface ConvidadoRepository extends JpaRepository<Convidado, Integer>{
-
-} 
